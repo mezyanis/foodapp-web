e6c4cc32cd5e894e31b10b997ff3378491ce2cca
Yanis <yanis.meziani@etu.univ-amu.fr>
2022-04-27 11:10:14 +0200

    je sais pas

--- server.js
+++ server.js
@@ -20,25 +20,17 @@ app.get("/search", async(req, res) => {
     res.sendFile(path.join(__dirname + '/views/Research.html'))
 
     //retourne toute les infos sur l'adress passée en parametre
-    const geocoding_options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com',
-            'X-RapidAPI-Key': 'acb9a3c9bfmsha48edd8b6c5047ap1075cdjsn5629b61b4d95'
-        }
-    };
 
     let user_adress = req.query.adress
-    let api_url = `https://trueway-geocoding.p.rapidapi.com/Geocode?address=${user_adress}&language=en`
-    let response = await fetch(api_url, geocoding_options)
-    let user_data = await response.json()
+    let user_data = await getGeoCode(user_adress)
+
+    console.log(user_data);
 
     //retourne toute les données sur la restaurant 
     let resto = model.return_restaurant()
-    let resto_adress = resto.adress
-    let api_url2 = `https://trueway-geocoding.p.rapidapi.com/Geocode?address=${resto_adress}&language=en`
-    let response_resto = await fetch(api_url2, geocoding_options)
-    let resto_data = await response_resto.json()
+    let resto_data = await getGeoCode(resto.adress)
+
+
 
 
     let user_coord = user_data.results[0].location
@@ -59,7 +51,32 @@ app.get("/search", async(req, res) => {
     let distance = await res_distance.json()
 
     console.log(distance);
-    console.log("1");
-    //res.json(distance)
+    //console.log("1");
+    //res.json(distance)*/
 })
+
+
+
+
+
+
+
+
+
+
+let getGeoCode = async(adress) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com',
+            'X-RapidAPI-Key': 'acb9a3c9bfmsha48edd8b6c5047ap1075cdjsn5629b61b4d95'
+        }
+    };
+
+    let api_url = `https://trueway-geocoding.p.rapidapi.com/Geocode?address=${adress}&language=en`
+    let response = await fetch(api_url, options)
+    return await response.json()
+
+
+}
 app.listen(3000, () => { console.log("server is running.."); })
